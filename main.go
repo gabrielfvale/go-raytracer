55278f653a217145abde2b46ce5a9dba06e5c8b2
Gabriel Freire <gabrielfvale@gmail.com>
2021-06-29 18:58:53 -0300

    Generating a PPM image

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	imageWidth, imageHeight := 256, 256
+
+	fmt.Println("P3")
+	fmt.Println(imageWidth, imageHeight)
+	fmt.Println(255)
+
+	for j := imageHeight - 1; j >= 0; j-- {
+		for i := 0; i < imageWidth; i++ {
+			r := float64(i) / float64(imageWidth-1)
+			g := float64(j) / float64(imageHeight-1)
+			b := 0.25
+
+			ir := int(255.99 * r)
+			ig := int(255.99 * g)
+			ib := int(255.99 * b)
+			fmt.Println(ir, ig, ib)
+		}
+	}
 }
