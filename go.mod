e85e4e9d382acf8ee301ca2db178e3c1686058b2
Gabriel Freire <gabrielfvale@gmail.com>
2021-06-30 10:08:11 -0300

    Add SDL2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gabrielfvale/go-traytracer
 
 go 1.15
+
+require github.com/veandco/go-sdl2 v0.4.8 // indirect
--- main.go
+++ main.go
@@ -1,24 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
 
 func main() {
-	imageWidth, imageHeight := 256, 256
 
-	fmt.Println("P3")
-	fmt.Println(imageWidth, imageHeight)
-	fmt.Println(255)
+	const w int = 512
+	const h int = 512
 
-	for j := imageHeight - 1; j >= 0; j-- {
-		for i := 0; i < imageWidth; i++ {
-			r := float64(i) / float64(imageWidth-1)
-			g := float64(j) / float64(imageHeight-1)
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("GO Raytracer", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		int32(w), int32(h), sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		panic(err)
+	}
+	defer renderer.Destroy()
+
+	texture, err := renderer.CreateTexture(sdl.PIXELFORMAT_RGB888, sdl.TEXTUREACCESS_STREAMING,
+		int32(w), int32(h))
+	if err != nil {
+		panic(err)
+	}
+	defer texture.Destroy()
+
+	pixels, pitch, err := texture.Lock(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	bpp := pitch / w // bytes-per-pixel
+	for j := 0; j < h; j++ {
+		for i := 0; i < w; i++ {
+			ind := (j * pitch) + (i * bpp)
+			r := float64(i) / float64(w-1)
+			g := float64(j) / float64(h-1)
 			b := 0.25
 
-			ir := int(255.99 * r)
-			ig := int(255.99 * g)
-			ib := int(255.99 * b)
-			fmt.Println(ir, ig, ib)
+			ir := uint8(255.99 * r)
+			ig := uint8(255.99 * g)
+			ib := uint8(255.99 * b)
+			pixels[ind] = ib   // B
+			pixels[ind+1] = ig // G
+			pixels[ind+2] = ir // R
+		}
+	}
+
+	texture.Update(nil, pixels, pitch)
+	texture.Unlock()
+
+	renderer.Clear()
+	renderer.Copy(texture, nil, nil)
+	renderer.Present()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				fmt.Println("Quit")
+				running = false
+				break
+			}
 		}
 	}
 }
